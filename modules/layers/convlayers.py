dcc1005f49b7e843bfa2a12c4f20508438864878
Jake <jacoby8s@gmail.com>
2023-05-29 16:28:26 -0700

    possibly conflicting padding with Dan edits. will merge if it is

--- modules/layers/convlayers.py
+++ modules/layers/convlayers.py
@@ -669,10 +669,10 @@ class STconvLayer(TconvLayer):
             s = x.reshape([-1] + self.input_dims).permute(4,1,0,2,3) # [1,C,B,W,H]
             w = w.reshape(self.filter_dims + [-1]).permute(4,0,3,1,2) # [N,C,T,W,H]
             
-             if self.padding:
+            if self.padding:
                  #pad = (self._npads[2], self._npads[3], self._npads[4], self._npads[5], self._npads[0], self._npads[1])
-                 pad = (self._npads[2], self._npads[3], self._npads[4], self._npads[5], self.filter_dims[-1]-1,0)
-             else:
+                pad = (self._npads[2], self._npads[3], self._npads[4], self._npads[5], self.filter_dims[-1]-1,0)
+            else:
                 # still need to pad the batch dimension
                 pad = (0,0,0,0,self.filter_dims[-1]-1,0)
 
