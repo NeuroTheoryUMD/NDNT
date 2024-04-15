ff7951300d1dba7321d733f438ffe54db37bba15
GccX11 <GccX11@users.noreply.github.com>
2024-04-15 12:35:40 -0400

    Adding documentation for how to generate the documentation.

--- __init__.py
+++ __init__.py
@@ -2,6 +2,11 @@
 ## Welcome to the documentation for NDNT!
 
 #### Click on the links on the left (or use the searchbar) to navigate through the documentation.
+
+To generate the documentation, run the following command in the parent directory of the repository:
+```bash
+pdoc -d google -o NDNT/docs NDNT
+```
 """
 
 from NDNT import modules
