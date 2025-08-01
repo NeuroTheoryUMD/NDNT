c7dace00144ef85d885c17be50f575269677f756
IsabelF98 <izyf98@gmail.com>
2025-08-01 16:47:50 -0400

    added simproj files

--- imports/imports.py
+++ imports/imports.py
@@ -66,6 +66,7 @@ import NDNT.utils as utils
 from NDNT.utils import fit_lbfgs
 from NDNT.utils.DanUtils import ss
 from NDNT.utils.DanUtils import imagesc
+from NDNT.utils.DanUtils import subplot_setup
 # NDNT -- base
 import NDNT.NDN as NDN
 from NDNT.modules.layers import *
@@ -100,7 +101,10 @@ def init_vars(globs, GPU = 0, project = None, datadir_input = None, dirname_inpu
     dirname = '/home/' + user + '/'    
     base_datadir = ''
     if project is None:
-        project = 'ColorV1'
+        if user.lower() == 'ifernand':
+            project = 'simcloud'
+        else:
+            project = 'ColorV1'
         print( "Project: ", project ) # display only if left blank so default is clear
 
     ########## Computer-specific data directories and GPU settings ##########
@@ -147,10 +151,14 @@ def init_vars(globs, GPU = 0, project = None, datadir_input = None, dirname_inpu
     ##### SimCloud project ######
     elif project.lower() == 'simcloud':
         #global SimCloudData, readout_fit
-        import NTdatasets.conway.synthcloud_datasets as syncloud
+        import NTdatasets.conway.synthcloud_datasets as scd
+        import ColorDataUtils.simproj_utils as spu
         import ColorDataUtils.readout_fit as readout_fit
-        globs['SimCloudData'] = syncloud
+        import ColorDataUtils.Barcode as BC
+        globs['scd'] = scd
+        globs['spu'] = spu
         globs['readout_fit'] = readout_fit
+        globs['BC'] = BC
 
         datadir = base_datadir + 'Antolik/'
     
